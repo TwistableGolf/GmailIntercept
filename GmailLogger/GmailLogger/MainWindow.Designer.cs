c4323c9710ae153f6df64803ce4e2d978f081287
TwistableGolf <lewissavage88@gmail.com>
2018-01-08 19:16:07 +0000

    Base Commit

---
+++ GmailLogger/GmailLogger/MainWindow.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace GmailLogger
+{
+    partial class GmailLogger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label toLabel;
+            this.recipientText = new System.Windows.Forms.TextBox();
+            this.bodyText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.attachFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.attachmentButton = new System.Windows.Forms.Button();
+            toLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // toLabel
+            // 
+            toLabel.AutoSize = true;
+            toLabel.Cursor = System.Windows.Forms.Cursors.Default;
+            toLabel.Location = new System.Drawing.Point(12, 40);
+            toLabel.Name = "toLabel";
+            toLabel.Size = new System.Drawing.Size(23, 13);
+            toLabel.TabIndex = 2;
+            toLabel.Text = "To:";
+            // 
+            // recipientText
+            // 
+            this.recipientText.Location = new System.Drawing.Point(64, 37);
+            this.recipientText.Name = "recipientText";
+            this.recipientText.Size = new System.Drawing.Size(100, 20);
+            this.recipientText.TabIndex = 0;
+            // 
+            // bodyText
+            // 
+            this.bodyText.Location = new System.Drawing.Point(64, 63);
+            this.bodyText.Multiline = true;
+            this.bodyText.Name = "bodyText";
+            this.bodyText.Size = new System.Drawing.Size(292, 132);
+            this.bodyText.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(312, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // attachFileDialog
+            // 
+            this.attachFileDialog.FileName = "Attachment";
+            this.attachFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.attachFileDialog_FileOk);
+            // 
+            // attachmentButton
+            // 
+            this.attachmentButton.Location = new System.Drawing.Point(80, 242);
+            this.attachmentButton.Name = "attachmentButton";
+            this.attachmentButton.Size = new System.Drawing.Size(75, 23);
+            this.attachmentButton.TabIndex = 4;
+            this.attachmentButton.Text = "Attach File";
+            this.attachmentButton.UseVisualStyleBackColor = true;
+            this.attachmentButton.Click += new System.EventHandler(this.Attachment_Click);
+            // 
+            // GmailLogger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 277);
+            this.Controls.Add(this.attachmentButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(toLabel);
+            this.Controls.Add(this.bodyText);
+            this.Controls.Add(this.recipientText);
+            this.Name = "GmailLogger";
+            this.Text = "Gmail Logger";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox recipientText;
+        private System.Windows.Forms.TextBox bodyText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog attachFileDialog;
+        private System.Windows.Forms.Button attachmentButton;
+    }
+}
+
---
+++ GmailLogger/GmailLogger/MainWindow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GmailLogger
+{
+    public partial class GmailLogger : Form
+    {
+        public GmailLogger()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Attachment_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void attachFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
